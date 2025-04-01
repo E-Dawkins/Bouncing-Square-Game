ac139457ce118aeb2e5fab17373bb35e57312bc6
E-Dawkins <ethandawkins@hotmail.co.uk>
2025-04-01 16:29:44 +1100

    Adjusted ContactGrow to grow a constant amount

--- Bouncing Squares/Assets/Scripts/Modifiers/ContactGrow.cs
+++ Bouncing Squares/Assets/Scripts/Modifiers/ContactGrow.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class ContactGrow : IModifier
 {
-    public float rate = 1.1f;
+    public float amount = 0.5f;
+    public Vector2 maxSize = new Vector2(3, 3);
 
     public ContactGrow(BouncingSquare owner) : base(owner)
     {
@@ -12,12 +13,14 @@ public class ContactGrow : IModifier
 
     public override void HandleCollision(CollisionData data) 
     {
-        owningSquare.transform.localScale *= rate;
+        Vector3 calculatedScale = owningSquare.transform.localScale + new Vector3(amount, amount, 1);
+        owningSquare.transform.localScale = Vector3.Min(calculatedScale, maxSize);
     }
 
     public override void CreateUI(SquareUIBuilder uiBuilder) 
     {
         base.CreateUI(uiBuilder);
-        uiBuilder.AddFloat("Rate", rate, (f) => { rate = f; }, new Vector2(1.1f, float.PositiveInfinity));
+        uiBuilder.AddFloat("Amount", amount, (f) => { amount = f; }, new Vector2(0.1f, 1));
+        uiBuilder.AddVec2("Max Size", maxSize, (v) => { maxSize = v; }, new Vector2(1.5f, 1.5f), new Vector2(10, 10));
     }
 }
